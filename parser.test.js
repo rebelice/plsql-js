8bb95453465e922abf2339a740a538c6ba20fce3
rebelice <yangrebelice@gmail.com>
2023-08-24 18:22:01 +0800

    chore: test shulk

--- parser.test.js
+++ parser.test.js
@@ -1,6 +1,7 @@
 import antlr4 from 'antlr4';
 import PlSqlLexer from './PlSqlLexer.js';
 import PlSqlParser from './PlSqlParser.js';
+import Autocompleter from 'antlr4-shulk/lib/autocompleter.js';
 
 test('two plus two is four', () => {
     expect(2 + 2).toBe(4);
@@ -13,4 +14,10 @@ test('parse simple SQL', () => {
     const tokens = new antlr4.CommonTokenStream(lexer);
     const parser = new PlSqlParser(tokens);
     const tree = parser.sql_script();
+});
+
+test ('auto complete simple SQL', () => {
+    const ac = new Autocompleter(PlSqlLexer, PlSqlParser);
+    const result = ac.autocomplete("SELECT * FROM t WHE");
+    console.log(result);
 });
